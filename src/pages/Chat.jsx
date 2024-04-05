daad8f1577cddb68437f58e9fa34d15e8259a62e
Sukoshi <hugo.cordon100@gmail.com>
2024-04-05 18:00:06 +0100

    Add refresh button

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -13,7 +13,7 @@ function Chat() {
     const [loginRequired, setLoginRequired] = useState(false);
     const [thisUser, setThisUser] = useState();
     const [selectedFriend, setSelectedFriend] = useState();
-    const [update, setUpdate] = useState();
+    const [update, setUpdate] = useState(false);
     const [inputValue, setInputValue] = useState('');
 
     const scrollableDivRef = useRef(null);
@@ -119,6 +119,10 @@ function Chat() {
       setInputValue(event.target.value);
     };
 
+    function refresh() {
+      setUpdate(!update)
+    }
+
     return (
         <>
             <Head logout={true}/>
@@ -146,6 +150,11 @@ function Chat() {
                 <div className="chatBackground">
                     <div className="chat" ref={scrollableDivRef}>
                         {selectedFriend ? <h2 className="selectedFriend">{selectedFriend}</h2> : <h2 className="selectedFriend">Select a friend to start chatting</h2>}
+                        <button className="refresh" onClick={refresh}>{
+                          <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M4.06189 13C4.02104 12.6724 4 12.3387 4 12C4 7.58172 7.58172 4 12 4C14.5006 4 16.7332 5.14727 18.2002 6.94416M19.9381 11C19.979 11.3276 20 11.6613 20 12C20 16.4183 16.4183 20 12 20C9.61061 20 7.46589 18.9525 6 17.2916M9 17H6V17.2916M18.2002 4V6.94416M18.2002 6.94416V6.99993L15.2002 7M6 20V17.2916" stroke="#999999" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                          </svg>
+                        }</button>
                         <div className="messages">
                           {messages.length ? messages.map((data) => (
                             <div key={data.user} className="message">
